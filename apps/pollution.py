fac837bd98cca5bc0e6ccea891f642bd889b90b1
Rohan <90356642+RohanDessaints@users.noreply.github.com>
2022-01-25 21:26:35 +0100

    petite modif de pollution

--- apps/pollution.py
+++ apps/pollution.py
@@ -38,7 +38,7 @@ def app():
     df_critair.reset_index(inplace=True)
     df_critair.rename(columns={'index': "Qualité_Air_Aujourd'hui", 0: 'Qualité_Air_Demain'}, inplace=True)
     df_critair["Qualité_Air_Aujourd'hui"] = df_critair["Qualité_Air_Aujourd'hui"].apply(lambda x: 'Bonne' if x == 'low'
-    else 'Moyenne' if x == 'average'
+    else 'Moyenne' if x ==                      'average'
     else 'Dégradée' if x == 'degrade'
     else 'Mauvaise' if x == 'high'
     else 'Très mauvaise' if x == 'very-high'
@@ -47,13 +47,13 @@ def app():
     df_critair["Qualité_Air_Demain"] = df_critair["Qualité_Air_Demain"].apply(lambda x: 'Bonne' if x == 'low'
     else 'Moyenne' if x == 'average'
     else 'Dégradée' if x == 'degrade'
-    else 'Mauvaise' if x == 'high'
+    else st.metric(label="demain",value='mauvaise') if x == 'high'
     else 'Très mauvaise' if x == 'very-high'
     else 'Inconnue' if x == '-'
     else 'Extrêmement mauvaise')
-    df_critair
 
-
+    #st.metric(label="demain",value='mauvaise')
+    #st.write(df_critair)
 
     def clean_data_belib():
         # création du DF dispo en temps réel
@@ -76,24 +76,27 @@ def app():
     df3 = df3[df3["fields.statut_pdc"] == "Disponible"].groupby(by="fields.code_insee_commune").count()
     df3.reset_index(inplace=True)
     df3.drop(columns=["fields.id_pdc"], inplace=True)
-    df3.rename(columns={"fields.code_insee_commune": "c_arinsee", "fields.statut_pdc": "bornes_dispo"}, inplace=True)
+    df3.rename(columns={"fields.code_insee_commune": "c_arinsee", "fields.statut_pdc": "bornes recharge disponibles"},
+               inplace=True)
     df3["c_arinsee"] = df3["c_arinsee"].astype(int)
     dfgeoA = gpd.read_file("arrondissements.geojson")
-    dfgeoA.keys()
     dfgeoA = pd.merge(dfgeoA, df3, on="c_arinsee")
-    dfgeoA
+    dfgeoA.rename(columns={"l_ar": "Arrondissement"}, inplace=True)
     lieu = adresse("10 rue du Louvre 75001".capitalize())
     carte_lieu = folium.Map(location=lieu, zoom_start=12)
-    folium.Choropleth(
+    cp = folium.Choropleth(
         geo_data=dfgeoA,
         name="geometry",
         data=dfgeoA,
-        columns=["c_arinsee", "bornes_dispo"],
+        columns=["c_arinsee", "bornes recharge disponibles"],
         key_on='feature.properties.c_arinsee',
-        fill_color='RdBu',
+        fill_color='YlGn',
         fill_opacity=0.6,
         line_opacity=0.6,
-        legend_name="Nombre de bornes recharges dispos par arrondissements").add_to(carte_lieu)
+        legend_name=("Nombre de bornes recharges dispos par arrondissements"),
+        highlight=True,
+        bins=4).add_to(carte_lieu)
     folium.raster_layers.TileLayer(tiles="Stamen Terrain", overlay=True).add_to(carte_lieu)
     folium.LayerControl('topright', collapsed=True).add_to(carte_lieu)
+    folium.GeoJsonTooltip(['Arrondissement', 'bornes recharge disponibles']).add_to(cp.geojson)
     folium_static(carte_lieu)
